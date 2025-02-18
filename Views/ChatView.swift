16f7ed9b9be439ad079629776e0a566f3f769823
jaydeep <viranijaydeep303@gmail.com>
2025-02-18 14:30:13 +0530

    pull

--- Views/ChatView.swift
+++ Views/ChatView.swift
@@ -38,22 +38,6 @@ struct ChatView: View {
                         .font(.custom("Montserrat-Bold", size: 32))
                         .foregroundStyle(.white)
                     Spacer()
-
-//                    Main branch
-//
-////                    Crash Branch gitHub Other branch
-//
-//                    NavigationLink(destination: AnalyticView()) {
-//                        Text("Crash!")
-//                            .font(.system(size: 20))
-//                            .foregroundStyle(.red)
-//                    }
-//
-////                    NavigationLink(destination: AnalyticView()) {
-////                        Text("Analytic!")
-////                            .font(.system(size: 20))
-////                            .foregroundStyle(.red)
-////                    }
                     Button(action: {
                         viewModal.signOut()
                     }, label: {
